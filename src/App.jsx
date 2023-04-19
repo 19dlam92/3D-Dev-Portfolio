5aa23d56ccbf666c5cb58ecd099075250764da4e
19dlam92 <19dlam92@gmail.com>
2023-04-19 12:19:47 -0400

    building components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {
+  BrowserRouter
+} from 'react-router-dom'
+import { 
+  About, 
+  Contact, 
+  Experience, 
+  Feedbacks, 
+  Hero, 
+  Navbar, 
+  Tech, 
+  Works, 
+  StarsCanvas 
+} from './components'
 
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
-    <div className="App">
-      3D Dev Portfolio
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg cover bg-no-repeat bg center'>
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    
+    </BrowserRouter>
   )
 }
 
