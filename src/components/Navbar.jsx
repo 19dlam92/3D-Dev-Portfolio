9bdbb2ecd342d0fb25a0af7d8063e2af29fbf1e4
19dlam92 <19dlam92@gmail.com>
2023-04-26 15:06:47 -0400

    modifying tailwind classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { logo, menu, close } from '../assets'
 const Navbar = () => {
 
   const [active, setActive] = useState("")
+  const [toggle, setToggle] = useState(false)
 
 
 
@@ -27,8 +28,54 @@ const Navbar = () => {
           >
             <img src={ logo } alt='logo' className='w-9 h-9 object-contain' />
             {/* import new logo in index.html within link href tag */}
-            <p className='text-white text-[18px] font-bold cursor-pointer'>David <span className='sm:block hidden'>| Portfolio</span></p>
+            <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+              David &nbsp; 
+              <span className='sm:block hidden'>| Portfolio</span>
+            </p>
           </Link>
+          <ul className='list-none hidden sm:flex flex-row gap-10'>
+            { navLinks.map((link) => (
+              <li 
+                key={ link.id } 
+                className={`${ 
+                  active === link.title 
+                  ? 'text-white' 
+                  : 'text-secondary'
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                onClick={() => setActive(link.title)}
+              >
+                <a href={`#${ link.id }`}>{ link.title }</a>
+              </li>
+            ))}
+          </ul>
+          <div className='sm:hiddem flex flex-1 justify-end items-center'>
+            <img 
+              src={ toggle ? close : menu } 
+              alt='menu' 
+              className='w-[28px] h-[28px] object-contain cursor-pointer'
+              onClick={() => setToggle(!toggle)}
+            />
+            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+              <ul className='list-none flex jsutify-end items-start flex-col gap-4'>
+              { navLinks.map((link) => (
+                <li 
+                  key={ link.id } 
+                  className={`${ 
+                    active === link.title 
+                    ? 'text-white' 
+                    : 'text-secondary'
+                  } font-poppins font-medium cursor-pointer text-[16px] `}
+                  onClick={() => {
+                    setToggle(!toggle)
+                    setActive(link.title)
+                  }}
+                >
+                  <a href={`#${ link.id }`}>{ link.title }</a>
+                </li>
+              ))}
+              </ul>
+            </div>
+          </div>
           <p className='text-red-500'>jibberjabber</p>
         </div>
       </nav>
@@ -36,4 +83,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
