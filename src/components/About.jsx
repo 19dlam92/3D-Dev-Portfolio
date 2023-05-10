da440272d4f7c7f59697e0ef4c2dfd58893f209a
19dlam92 <19dlam92@gmail.com>
2023-05-10 16:28:00 -0400

    debugging 3d model

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import Tilt from 'react-tilt'
+import Motion from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
 
 const About = () => {
   return (
