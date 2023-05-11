6c593c20729ad5a92502ebe2d3d4634998e9be8a
19dlam92 <19dlam92@gmail.com>
2023-05-11 18:33:37 -0400

    creating about me component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,42 @@ import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
+const ServiceCard = ({ index, title, icon }) => {
+  return(
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={ fadeIn('right', 'spring', 0.5 * index, 0.75) }
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+
+      </motion.div>
+    </Tilt>
+  )
+}
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={ textVariant }>
+        <p className={ styles.sectionSubText }>Introduction</p>
+        <h2 className={ styles.sectionHeadText }>Overview</h2>
+      </motion.div>
+      <motion.p
+        variants= { fadeIn('', '', 0.1, 1) }
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Description about what I develop . . . .
+      </motion.p>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        { services.map((service, index) => (
+          <ServiceCard 
+            key={ service.title }
+            index={ index }
+            { ...service}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
